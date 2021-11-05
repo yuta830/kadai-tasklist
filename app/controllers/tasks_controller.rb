833c71479d0e778d99a87abdd6f351ce82e9a6df
yuta.kato <yyyutaa7@gmail.com>
2021-11-05 15:50:44 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end 
   
   def show
-    @task = Task.find(params[:id])
   end 
   
   def new
@@ -48,6 +48,11 @@ class TasksController < ApplicationController
   end
   
   private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  
   def task_params
     params.require(:task).permit(:content)
   end
